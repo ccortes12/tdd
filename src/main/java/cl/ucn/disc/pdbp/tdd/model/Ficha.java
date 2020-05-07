2bc3b2c4035f06ef08104e7fd0d81a9faa1ee6dc
Carlos Cortés González <carlos.cortes@alumnos.ucn.cl>
2020-05-07 06:57:12 -0400

    feat(Domain): update the model

--- src/main/java/cl/ucn/disc/pdbp/tdd/model/Ficha.java
+++ src/main/java/cl/ucn/disc/pdbp/tdd/model/Ficha.java
@@ -16,10 +16,116 @@
 
 package cl.ucn.disc.pdbp.tdd.model;
 
+import java.time.ZonedDateTime;
+
 /**
- * author Carlos Cortes
+ * The Ficha veterinaria
+ *
+ * @author Carlos Cortes
  */
 public class Ficha {
 
+    /**
+     * The numero of ficha
+     */
+    private final Integer numeroFicha;
 
+    /**
+     * The nombre
+     */
+    private final String nombrePaciente;
+
+    /**
+     * The especie
+     */
+    private final String especie;
+
+    /**
+     * The fecha de nacimiento
+     */
+    private final ZonedDateTime fechaNaciemiento;
+
+    /**
+     * Raza
+     */
+    private final String raza;
+
+    /**
+     * Color
+     */
+    private final String color;
+
+    /**
+     * Tipo: externo/interno
+     */
+    private final Tipo tipo;
+
+    /**
+     * The constructor
+     */
+    public Ficha(Integer numeroFicha, String nombrePaciente, String especie, ZonedDateTime fechaNaciemiento, String raza, String color, Tipo tipo) {
+        this.numeroFicha = numeroFicha;
+        this.nombrePaciente = nombrePaciente;
+        this.especie = especie;
+        this.fechaNaciemiento = fechaNaciemiento;
+        this.raza = raza;
+        this.color = color;
+        this.tipo = tipo;
+    }
+
+    /**
+     *
+     * @return the numeroFicha
+     */
+    public Integer getNumeroFicha() {
+        return numeroFicha;
+    }
+
+    /**
+     *
+     * @return the nombrePaciente
+     */
+    public String getNombrePaciente() {
+        return nombrePaciente;
+    }
+
+    /**
+     *
+     * @return the especie
+     */
+    public String getEspecie() {
+        return especie;
+    }
+
+    /**
+     *
+     * @return the fechaNacimiento
+     */
+    public ZonedDateTime getFechaNaciemiento() {
+        return fechaNaciemiento;
+    }
+
+    /**
+     *
+     * @return the raza
+     */
+    public String getRaza() {
+        return raza;
+    }
+
+    /**
+     *
+     * @return the color
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     *
+     * @return the tipo
+     */
+    public Tipo getTipo() {
+        return tipo;
+    }
 }
